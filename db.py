b1e71dc0291a04bb5923e4f440d87bb23185253f
Timur Khuzhin <timrock@MacBook-Air-Timur.local>
2022-06-04 22:05:48 +0300

    added database

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+DB_NAME = 'dacha.db'
+
+
+def db_connection(func):
+    """
+    Decorator for secure connection to sqlite using context manager.
+    Creates connection to DB if not exist and returns it.
+    """
+
+    def wrapper(*args, **kwargs):
+        with sqlite3.connect(DB_NAME) as conn:
+            kwargs['conn'] = conn
+            res = func(*args, **kwargs)
+        return res
+
+    return wrapper
+
+
+@db_connection
+def _init_db(conn: sqlite3.Connection, force: bool = False):
+    cursor = conn.cursor()
+    if force:
+        cursor.execute('DROP TABLE IF EXISTS user_category')
+        cursor.execute('DROP TABLE IF EXISTS category')
+        cursor.execute('DROP TABLE IF EXISTS user')
+
+    with open('createdb.sql', 'r') as f:
+        sql = f.read()
+    cursor.executescript(sql)
+    conn.commit()
+
+
+if __name__ == '__main__':
+    _init_db()
